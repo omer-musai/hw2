8e96f5876328e1b61c32b3077ea7611b4a801985
Shay <ShayHacohen14@gmail.com>
2021-06-08 16:54:32 +0300

    Wrapping in namespace mtm instead of using. Replacing the Month enum with a simple integer. Adding a couple of consts. Adding a couple of explicit int-float casts.

--- examDetails.cpp
+++ examDetails.cpp
@@ -1,13 +1,13 @@
 #include "examDetails.h"
 
 
-using namespace mtm;
-
-ExamDetails::ExamDetails(int course_number, Month month, int day, float hour, int duration, std::string link)
+namespace mtm
 {
-        int rounded_hour = hour;
+    ExamDetails::ExamDetails(int course_number, int month, int day, float hour, int duration, const std::string& link)
+    {
+        int rounded_hour = (int)hour;
 
-        if(day <= 0 || day > DAYS_IN_MONTH || month < JANUARY || month > DECEMBER)
+        if(day <= 0 || day > DAYS_IN_MONTH || month < 1 || month > 12)
         {
             throw InvalidDateException();
         }
@@ -26,93 +26,81 @@ ExamDetails::ExamDetails(int course_number, Month month, int day, float hour, in
         this->hour = hour;
         this->duration = duration;
         this->link = link;
-}
+    }
 
-
-ExamDetails::ExamDetails(const ExamDetails& exam) : 
-     course_number(exam.course_number), month(exam.month), day(exam.day), hour(exam.hour), duration(exam.duration), link(exam.link)
-{
-    //everything copied
-}
-
-ExamDetails::~ExamDetails()
-{
-    //nothing to do basically
-}
-
-ExamDetails& ExamDetails::operator=(const ExamDetails& exam)
-{
-    if(this == &exam)
+    ExamDetails& ExamDetails::operator=(const ExamDetails& exam)
     {
+        if(this == &exam)
+        {
+            return *this;
+        }
+        course_number = exam.course_number;
+        day = exam.day;
+        month = exam.month;
+        hour = exam.hour;
+        duration = exam.duration;
+        link = exam.link;
+
         return *this;
     }
-    course_number = exam.course_number;
-    day = exam.day;
-    month = exam.month;
-    hour = exam.hour;
-    duration = exam.duration;
-    link = exam.link;
 
-    return *this;
-}
-
-ExamDetails ExamDetails::makeMatamExam()
-{
-    ExamDetails exam(MATAM_COURSE_NUM, JULY, MATAM_DAY, MATAM_HOUR, MATAM_DURATION, MATAM_LINK);
-    return exam;
-}
-
-std::string ExamDetails::getLink()
-{
-    return this->link;
-}
-
-
-void ExamDetails::setLink(std::string link)
-{
-    this->link = link;
-}
-
-   
-int ExamDetails::operator-(const ExamDetails& exam) const
-{
-    int month_diff = this->month - exam.month;
-
-    return(this->day - exam.day + (month_diff * DAYS_IN_MONTH));
-}
-
-bool ExamDetails::operator<(const ExamDetails& exam) const
-{
-    if(this->month == exam.month)
+    ExamDetails ExamDetails::makeMatamExam()
     {
-        if(this->day == exam.day)
+        ExamDetails exam(MATAM_COURSE_NUM, MATAM_MONTH, MATAM_DAY, MATAM_HOUR, MATAM_DURATION, MATAM_LINK);
+        return exam;
+    }
+
+    std::string ExamDetails::getLink() const
+    {
+        return this->link;
+    }
+
+
+    void ExamDetails::setLink(const std::string& new_link)
+    {
+        this->link = new_link;
+    }
+
+
+    int ExamDetails::operator-(const ExamDetails& exam) const
+    {
+        int month_diff = this->month - exam.month;
+
+        return(this->day - exam.day + (month_diff * DAYS_IN_MONTH));
+    }
+
+    bool ExamDetails::operator<(const ExamDetails& exam) const
+    {
+        if(this->month == exam.month)
         {
-           return (this->hour < exam.hour); //return false if equal as required
+            if(this->day == exam.day)
+            {
+                return (this->hour < exam.hour); //return false if equal as required
+            }
+
+            return (this->day < exam.day);
         }
-       
-       return (this->day < exam.day);
+
+        return (this->month < exam.month);
     }
 
-    return (this->month < exam.month);
-}
 
-
-std::ostream& mtm::operator<<(std::ostream& os, const ExamDetails& exam)
-{
-    std::string minutes = "00";
-    int rounded_hour = exam.hour;
-    
-    if(exam.hour > rounded_hour)
+    std::ostream& operator<<(std::ostream& os, const ExamDetails& exam)
     {
-        minutes = "30";
+        std::string minutes = "00";
+        int rounded_hour = (int)exam.hour;
+
+        if(exam.hour > (float)rounded_hour)
+        {
+            minutes = "30";
+        }
+
+        os << "\n"
+           << "Course Number: " << exam.course_number << "\n"
+           << "Time: " << exam.day << "." << exam.month << "at" << rounded_hour << ":" << minutes << "\n"
+           << "Duration: "<< exam.duration << ":" << "00" << "\n"
+           << "Zoom Link: " << exam.link;
+
+        return os;
     }
-
-    os << "\n"
-        << "Course Number: " << exam.course_number << "\n"
-        << "Time: " << exam.day << "." << exam.month << "at" << rounded_hour << ":" << minutes << "\n"
-        << "Duration: "<< exam.duration << ":" << "00" << "\n"
-        << "Zoom Link: " << exam.link;
-
-    return os;
 }
-
