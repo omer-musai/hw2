5a62ad7375f5e2e4486ff3b351796b562b261fa4
Shay <ShayHacohen14@gmail.com>
2021-06-14 22:28:41 +0300

    Fixing the Exception-subclasses error messages.

--- pt2/part_b/Exceptions.cpp
+++ pt2/part_b/Exceptions.cpp
@@ -3,10 +3,12 @@
 namespace mtm
 {
     Exception::Exception(const std::string& error_type) : error_type(error_type)
-    {}
+    {
+        error_message = ("A game related error has occurred: " + error_type);
+    }
 
     const char* Exception::what() const noexcept {
-        return ("A game related error has occurred: " + error_type).c_str();
+        return error_message.c_str();
     }
 
     IllegalArgument::IllegalArgument()
